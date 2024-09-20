7f96cd696d0923211d2110649da0a38b78c89a3c
dbasrai <diyabasrai@gmail.com>
2024-09-20 12:04:28 -0500

    lunch!

--- quest_scripts/myFirstScript.py
+++ quest_scripts/myFirstScript.py
@@ -3,18 +3,19 @@ from src.utils import *
 
 
 
-num_workers = 10
+num_workers =10
 
 my_data=[]
 bias=10
 
-for i in range(num_workers):
-    my_data.append(np.random.rand(100,100,100))
+for i in range(100):
+    my_data.append(np.random.rand(5000,100))
 
-def covary(matrix, bias):
-    return matrix@matrix.T + bias
+def covinv(matrix, bias):
+    cov = matrix.T@matrix
+    return np.linalg.inv(cov)+bias
 
-output = multipool(covary, bias, iterable=my_data, num_workers=num_workers)
+output = multipool(covinv, bias, iterable=my_data, num_workers=num_workers)
 
 pdump(output, '../picklejar/big_discovery.pickle')
 
